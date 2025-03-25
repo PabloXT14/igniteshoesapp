908d362318ec0f54e52a374d053ade825596af6c
Pablo Alan <pabloxt14@gmail.com>
2025-03-25 18:40:01 -0300

    feat: create delete tag function

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,31 @@
 import { Feather } from '@expo/vector-icons'
-import { Heading, HStack, Icon, Text, VStack, Avatar } from 'native-base'
+import {
+  Heading,
+  HStack,
+  Icon,
+  Text,
+  VStack,
+  Avatar,
+  useToast,
+} from 'native-base'
 
 import userPhotoDefault from '../assets/userPhotoDefault.png'
+import { TouchableOpacity } from 'react-native'
+import { deleteTagUserEmail } from '@/notifications/notificationsTags'
 
 export function HomeHeader() {
+  const toast = useToast()
+
+  function handleSignOut() {
+    deleteTagUserEmail()
+
+    toast.show({
+      placement: 'top',
+      bgColor: 'green.500',
+      title: 'Tag de notificação excluída',
+    })
+  }
+
   return (
     <HStack pt={16} pb={5} px={8} bg="gray.600" alignItems="center">
       <Avatar
@@ -24,7 +46,9 @@ export function HomeHeader() {
         </Heading>
       </VStack>
 
-      <Icon as={Feather} name="log-out" color="gray.200" size={7} />
+      <TouchableOpacity onPress={handleSignOut}>
+        <Icon as={Feather} name="log-out" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   )
 }
--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -3,3 +3,7 @@ import { OneSignal } from 'react-native-onesignal'
 export function createTagUserEmail(email: string) {
   OneSignal.User.addTag('user_email', email)
 }
+
+export function deleteTagUserEmail() {
+  OneSignal.User.removeTag('user_email')
+}
