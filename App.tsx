fb596737785dfb5afef5be557b136189d3f8a1da
Pablo Alan <pabloxt14@gmail.com>
2025-04-03 17:59:36 -0300

    feat: verify click in notification on background

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
 import {
@@ -5,7 +6,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
-import { OneSignal } from 'react-native-onesignal'
+import { type NotificationClickEvent, OneSignal } from 'react-native-onesignal'
 
 import { Routes } from './src/routes'
 
@@ -22,6 +23,20 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   createUserInfoTags()
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent) => {
+      console.log('NOTIFICATION CLICKED: ', event)
+    }
+
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick)
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        'click',
+        handleNotificationClick
+      )
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
